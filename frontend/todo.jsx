d52687414c4285182653c753aad78a75e8e1e40c
David Harris <dahveed15@gmail.com>
2018-08-19 21:23:44 -0700

    reducers/store works

--- frontend/todo.jsx
+++ frontend/todo.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+// import { fetchTodos } from './util/todo_list_api_util';
+// import { receiveAllTodos } from './actions/todo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore();
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.fetchTodos = fetchTodos;
+  // window.receiveAllTodos = receiveAllTodos;
   const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Todo App</h1>, rootEl);
 });
