5c6ddbda04fea19a13438243c0ffd20a33ecbd3f
David Harris <dahveed15@gmail.com>
2018-08-20 10:55:08 -0700

    root component renders correctly

--- frontend/todo.jsx
+++ frontend/todo.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 // import { fetchTodos } from './util/todo_list_api_util';
 // import { fetchTodos } from './actions/todo_actions';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
@@ -11,5 +12,5 @@ document.addEventListener('DOMContentLoaded', () => {
   // window.fetchTodos = fetchTodos;
   // window.receiveAllTodos = receiveAllTodos;
   const rootEl = document.getElementById('root');
-  ReactDOM.render(<h1>Todo App</h1>, rootEl);
+  ReactDOM.render(<Root store={store} />, rootEl);
 });
